5dbd42601129127c7c7be4630f2ec1b7b4660289
Daniwl <150160314+fukorpse@users.noreply.github.com>
2025-04-03 12:37:57 -0300

    Add files via upload

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput, IonButton, IonList, IonLabel, IonIcon } from '@ionic/angular/standalone';
-
 //Função para adicionar o icone
 
 import { addIcons } from 'ionicons';
@@ -10,6 +9,7 @@ import { addIcons } from 'ionicons';
 //Importar o icone
 
 import {ellipseOutline,checkmarkCircle, eye, eyeOff, trash } from 'ionicons/icons'
+import { TasksService } from '../service/tasks.service';
 
 //Interfaca para criar um "objeto" com nossa tarefa
 interface Task {
@@ -30,7 +30,23 @@ export class HomePage {
 
   exibirConcluidos: boolean = true; // Variavel de controle para exibir ou ocultar
 
-  addTask() {
+  // addTask() {
+  //   if (this.newTask.trim()) {
+
+  //     const newTaskObj: Task = { // Cria objeto Task
+  //       id: Date.now(),
+  //       title: this.newTask.trim(),
+  //       completed: false
+  //     };
+  //     this.tasks.push(newTaskObj);
+
+  //     this.newTask = '';
+      
+  //   }
+    
+  // }
+
+  async addTask() {
     if (this.newTask.trim()) {
 
       const newTaskObj: Task = { // Cria objeto Task
@@ -38,13 +54,15 @@ export class HomePage {
         title: this.newTask.trim(),
         completed: false
       };
-      this.tasks.push(newTaskObj);
+      
+      // this.tasks.push(newTaskObj);
+      await this.taskService.addTask(newTaskObj);
+
+      this.tasks = await this.taskService.getTask();
 
       this.newTask = '';
       
     }
-
-
     
   }
 
@@ -67,7 +85,7 @@ export class HomePage {
   }
 
 
-  constructor() {
+  constructor(private taskService: TasksService) {
 
     addIcons({ellipseOutline, checkmarkCircle, eye, eyeOff, trash});
    }
--- src/app/service/tasks.service.ts
+++ src/app/service/tasks.service.ts
@@ -1,19 +1,38 @@
 import { Injectable } from '@angular/core';
 
-import {Preferences } from '@capacitor/preferences'
+import { Preferences } from '@capacitor/preferences'
+
 
 export interface Task{
   id: number,
   title: string,
-  completed:boolean
+  completed: boolean
 }
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class TasksService {
 
+  private TASK_KEY = 'task';
 
+  async addTask(task:Task): Promise<void>{
+
+    const tasks = await this.getTask() || [];
+    tasks.push(task);
+
+    await Preferences.set({
+      key: this.TASK_KEY,
+      value: JSON.stringify(task)
+    });
+
+  }
+
+  async getTask(): Promise<Task[]>{
+    const {value} = await Preferences.get({key: this.TASK_KEY});
+    return value ? JSON.parse(value) : [];
+  }
 
 
   constructor() { }
